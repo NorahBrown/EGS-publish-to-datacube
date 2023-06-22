aeb27059f661f3a52129f7cca316a8a57d63e69c
Xinli Cai <103012417+xinli-cai@users.noreply.github.com>
2023-06-22 09:32:14 -0400

    Add additional parameters to the cog creation

--- COG_creation/geotiff_to_cog.py
+++ COG_creation/geotiff_to_cog.py
@@ -1,3 +1,4 @@
+import os 
 from osgeo import gdal
 def print_gdal_info(file_path, print_keys=True):
     """"
@@ -27,7 +28,11 @@ def reproject_raster(input_path, dstSRS, xRes, yRes):
         format='GTiff', 
         dstSRS = dstSRS, 
         xRes=xRes, 
-        yRes=yRes
+        yRes=yRes, 
+        targetAlignedPixels=True, 
+        resampleAlg = 'near' , 
+        srcNodata=0,
+        dstNodata=0  
     )
     ds = gdal.Warp(destNameOrDestDS=reProj_path, srcDSOrSrcDSTab=input_path, options=warp_options)
     
@@ -50,25 +55,30 @@ def geotiff_to_cog(input_path, output_path, datetime_value):
                          'BLOCKSIZE = 512', 
                          'BLOCKSIZE = 512'
                          ],
+        # Add datetime tag while creating the COG 
+        metadataOptions=[f'TIFFTAG_DATETIME = {datetime_value}']
         )    
     # Translate the TIFF to COG
     ds = gdal.Translate(output_path, input_path, options=translate_options)    
-    
+    """
     # Add TIFFTAG_DATATIME 
     try: 
         if ds is not None: 
             ds.SetMetadataItem("TIFFTAG_DATETIME", datetime_value)
     except Exception as e: 
         print(f"gdal.Translate returns a None object created an error when setting TIFFTAG_DATETIME for {input_path}: {str(e)}")
-    
+    """
     # Close the data
     ds = None 
     
-    
 # Usage
-input_path ='.../RiverIce_CAN_NT_UpperMackenzieMillsLake_20210429_012858.tif'
-output_path = '.../RiverIce_CAN_NT_UpperMackenzieMillsLake_20210429_012858_cog.tif'
+script_dir = os.path.dirname(os.path.abspath(__file__))
+input_path =os.path.join(script_dir,  'Test', 'tiff', 'RiverIce_CAN_ON_Moose_20160503_232950.tif')
+output_path = input_path.replace('.tif', '_cog.tif')
 
 reproject_raster(input_path, dstSRS='EPSG:3978', xRes=5, yRes=5)
 proj_path=input_path.replace('.tif', '_reprj.tif')
 print_gdal_info(proj_path, print_keys=True)
+
+geotiff_to_cog(input_path, output_path, datetime_value='2016:05:03 23:29:50')
+print_gdal_info(output_path, print_keys=True)
